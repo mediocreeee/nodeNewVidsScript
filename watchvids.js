acf48fd7cdc55206dc9fdb4edea2d87885a060c9
mediocre <druksasha@ukr.net>
2020-04-30 04:32:00 +0300

    async await

--- watchvids.js
+++ watchvids.js
@@ -1,98 +1,93 @@
 #!/usr/bin/env node
 
-// TODO: refactor functions
+// TODO: refactor in functions
 // TODO: maybe rewrite it not using youtube api
+// TODO: error handling
+// TODO: folder navigation
 
-(async() => {
-  const fetch = require('node-fetch')
-  const fs = require('fs')
-  const os = require('os')
-  const opn = require('opn')
+(async () => {
+  const fetch = require("node-fetch");
+  const fs = require("fs");
+  const os = require("os");
+  const opn = require("opn");
+  const util = require("util")
+
+  const readFile = util.promisify(fs.readFile)
+  const writeFile = util.promisify(fs.writeFile)
 
   // User settings
-  const user = '' // Your system user nickname
-  const browser = '' // Browser in which you want to open videos
-  const channelIds = { // Your channels list to follow
-	'lukesmith': 'UC2eYFnH61tmytImy1mTYvhA', // ex: 'random name what u like': 'channel id (in the end of url on the channel's main page)'
-	'dt': 'UCVls1GmFKf6WlTraIb_IaJg',
-	'extremecode': 'UCBNlINWfd08qgDkUTaUY4_w',
-	'kell': 'UCfn7uyPvr5IY5pux8oyIa4Q',
-	'later': 'UCfdgIq01iG92AXBt-NxgPkg',
-	'kultas': 'UCDM0Ng48_zAMTazoC_XAsaA',
-	'slizen': 'UCr1Pf6rqk3h8b1APvAt42Bw',
-  }
+  const user = ""; // Your system user nickname
+  const browser = ""; // Browser in which you want to open videos
+  const channelIds = {
+	// Your channels list to follow
+	extremecode: "UCBNlINWfd08qgDkUTaUY4_w",
+  };
 
   // Script setup
-  const homedir = os.homedir()
+  const homedir = os.homedir();
   const apiKey = '' // Your YoutubeDataAPI key
-  const baseVideoUrl = 'https://www.youtube.com/watch?v='
-  const baseSearchUrl = 'https://www.googleapis.com/youtube/v3/search?'
-  const youtubers = {}
+  const baseVideoUrl = "https://www.youtube.com/watch?v=";
+  const baseSearchUrl = "https://www.googleapis.com/youtube/v3/search?";
+  const youtubers = {};
 
   // Check if the file with channels and videos already exists
-  if(!fs.existsSync(`${homedir}/scripts/watchvids/youtube.json`)) {
-	fs.writeFile(`${homedir}/scripts/watchvids/youtube.json`,'', (err) => {
-	  if (err) throw err
-	})
+  if (!fs.existsSync(`${homedir}/scripts/watchvids/youtube.json`)) {
+	fs.writeFile(`${homedir}/scripts/watchvids/youtube.json`, "", (err) => {
+	  if (err) throw err;
+	});
   }
 
   // Iteration on users channels list and openning new videos in browser
-  for (const id in channelIds){
-
+  for (const id in channelIds) {
 	// Video url
-	const url = `${baseSearchUrl}key=${apiKey}&channelId=${channelIds[id]}&part=snippet,id&order=date&maxResults=1`
+	const url = `${baseSearchUrl}key=${apiKey}&channelId=${channelIds[id]}&part=snippet,id&order=date&maxResults=1`;
 
 	// Getting data from YoutubeDataAPI
-	const getYoutubeJSON = async url => {
+	const getYoutubeJSON = async (url) => {
 	  try {
-		const res = await fetch(url)
-		const json = await res.json()
-		return json
-	  }catch(err) {
-		console.log(err)
+		const res = await fetch(url);
+		const json = await res.json();
+		return json;
+	  } catch (err) {
+		console.log(err);
 	  }
-	}
-	const youtubeJSON = await getYoutubeJSON(url)
-	const videoId = youtubeJSON['items'][0]['id']['videoId']
-	const channelName = youtubeJSON['items'][0]['snippet']['channelTitle']
+	};
+	const youtubeJSON = await getYoutubeJSON(url);
+	const videoId = youtubeJSON["items"][0]["id"]["videoId"];
+	const channelName = youtubeJSON["items"][0]["snippet"]["channelTitle"];
 
 	// File write schema
-	const youtuber = {
-	  channelName: {
-		lastVidId: videoId
+	youtubers[channelName] = videoId;
+
+	// Reading youtubers.json
+	const previousVideoData = await readFile(`${homedir}/scripts/watchvids/youtube.json`, "utf-8")
+	  .catch(err => console.log(err))
+
+	// Check if file is empty
+	if (previousVideoData == ""){
+	  await opn(`${baseVideoUrl}${videoId}`, { app: browser });
+	  let newVideoData = await JSON.stringify(youtubers, null, 2)
+	  await writeFile(`${homedir}/scripts/watchvids/youtube.json`, newVideoData)
+		.then( () => {
+		  console.log(
+			`Found new video on channel ${channelName}! Openning...`
+		  )
+		})
+		.catch(err => console.log(err))
+	} else {
+	  // If not read previous data and match with new
+	  let youtubersJSON = await JSON.parse(previousVideoData);
+	  let lastVideoId = youtubersJSON[channelName];
+	  if (lastVideoId != videoId) {
+		console.log(`Found new video on channel ${channelName}! Openning...`)
+		await opn(`${baseVideoUrl}${videoId}`, { app: browser });
+	  } else {
+		console.log(`No new vids on channel ${channelName}`);
 	  }
 	}
-	youtubers[channelName] = videoId
-
-	fs.readFile(`${homedir}/scripts/watchvids/youtube.json`, 'utf-8', (err, previousVideoData) => {
-	  if(err) throw err
-
-	  // Check if file is empty
-	  if(previousVideoData == ''){
-		opn(`${baseVideoUrl}${videoId}`, {app: browser})
-		let newVideoData = JSON.stringify(youtubers, null, 2)
-		fs.writeFile(`${homedir}/scripts/watchvids/youtube.json`, newVideoData, (err) => {
-		  if (err) throw err
-		  console.log(`Found new video on channel ${channelName}! Openning...`)
-		})
-		return
-	  }
-	  // If the file exists read previous data and match with new
-	  let youtubersJSON = JSON.parse(previousVideoData)
-	  let lastVideoId = youtubersJSON[channelName]
-	  if(lastVideoId != videoId){
-		opn(`${baseVideoUrl}${videoId}`, {app: browser})
-		let newVideoData = JSON.stringify(youtubers, null, 2)
-		fs.writeFile(`${homedir}/scripts/watchvids/youtube.json`, newVideoData, (err) => {
-		  if (err) throw err
-		  console.log(`Found new video on channel ${channelName}! Openning...`)
-		})
-	  }else{
-		console.log(`No new vids on channel ${channelName}`)
-	  }
-	})
   }
 
-})()
-
-
+  // Write newly generated data into file
+  let newVideoData = await JSON.stringify(youtubers, null, 2);
+  await writeFile(`${homedir}/scripts/watchvids/youtube.json`, newVideoData).catch( err => console.log(err))
+})();
